2fb4dbcd2780974b8527698d2296e91f6ebcc820
KevinDeron <kevin.deron@hotmail.com>
2026-09-03 18:17:21 -0400

    fix erro de logica

--- cardapio.c
+++ cardapio.c
@@ -5,27 +5,31 @@
 #include "cardapio.h"
 
 int totalItensCardapio = 0;
+struct item *cardapio;
 
 int adicionarItemCardapio(char *nome, float preco){
     int indiceN;
     if (strlen(nome) >= 14){
         printf("Nome do item maior que %zu caracteres", sizeof(cardapio->nome) + 1);
         return 1;
-    }    
-    for(indiceN = 0;indiceN <= totalItensCardapio;indiceN++){
-        if(strcmp(cardapio[indiceN].nome, nome) == 0){
-            printf("Item ja existe");
-            return 1;
+    }
+    if(totalItensCardapio != 0){
+        for(indiceN = 0;indiceN <= totalItensCardapio;indiceN++){
+            if(strcmp(cardapio[indiceN].nome, nome) == 0){
+                printf("Item ja existe");
+                return 1;
+            }
         }
     }
+    totalItensCardapio++;
     struct item *new_p = realloc(cardapio, totalItensCardapio * sizeof(struct item));
     if(new_p == NULL){
         printf("Falha ao alocar memoria![Item]");
-        return 0;
+        totalItensCardapio--;
+        return 1;
     }
     cardapio = new_p;
     strcpy(cardapio[totalItensCardapio].nome, nome);
     cardapio[totalItensCardapio].preco = preco;
-    totalItensCardapio++;
     return 0;
 }
